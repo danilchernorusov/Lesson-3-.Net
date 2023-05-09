2600f336852d68dc8f25e0555e3d9ae8b5d6800c
Danil Chernorusov <danilchernorusov@gmail.com>
2023-05-09 11:35:39 +0300

    1 commit

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int maxNumber, minNumber;
+        Console.Write("Введiть максимальне значеня дiапазону: ");
+        maxNumber = Convert.ToInt32(Console.ReadLine());
+        do
+        {
+            Console.Write("Введiть мiнемальне значеня дiапазону: ");
+            minNumber = Convert.ToInt32(Console.ReadLine());
+        } while (minNumber >= maxNumber);
+
+
+        for (int i = maxNumber; i >= minNumber; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string number;
+        int sumOfDigits = 0;
+        do
+        {
+            Console.Write("Введiть число в дiапазонi вiд 1 до 1000000000: ");
+            number = Console.ReadLine();
+        } while (Convert.ToInt32(number) < 1 | Convert.ToInt32(number) > 1000000000);
+
+
+        for (int i = 0; i < number.Length; i++) 
+        {
+            sumOfDigits += Convert.ToInt32(number[i].ToString());
+        }
+
+        Console.WriteLine(sumOfDigits);
+    }
+}
